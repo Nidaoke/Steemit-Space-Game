674f716aa31316d27da513f87aac2a692db512ae
Nidolabz <jnidaokefantagefacebook27@gmail.com>
2018-01-14 18:00:16 -0500

    Added Scrolling code

--- Assets/VertScrollingCode.cs
+++ Assets/VertScrollingCode.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class VertScrollingCode : MonoBehaviour {
 
 	public float speed;
+	public bool starting;
+	public VertScrollingCode otherOne;
+
+	private float lessFloat;
+	private float setFloat;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (starting) {
+			setFloat = otherOne.GetStartingY ();
+			lessFloat = (transform.position.y - (otherOne.GetStartingY () - transform.position.y));
+			otherOne.SetLessFloat (lessFloat);
+			otherOne.SetSetFloat (setFloat);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position += new Vector3 (0, -speed, 0);
+
+		if (transform.position.y <= lessFloat) {
+			transform.position = new Vector3 (0, setFloat, 0); 
+		}
+	}
+
+	public float GetStartingY(){
+		return transform.position.y;
+	}
+	public void SetLessFloat(float newFloat){
+		lessFloat = newFloat;
+	}
+	public void SetSetFloat(float newFloat){
+		setFloat = newFloat;
 	}
 }
