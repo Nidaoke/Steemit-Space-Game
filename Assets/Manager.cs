c44dff06017f201422d90a30598e6dd65738c1ca
Nidolabz <jnidaokefantagefacebook27@gmail.com>
2018-01-14 18:16:12 -0500

    Added Singleton Class, a manager with globals, and increased speed on loops

---
+++ Assets/Manager.cs
@@ -0,0 +1,12 @@
+﻿public class Manager : Singleton<Manager> {
+	protected Manager () {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	//public string testGlobalVar = "test!";
+	public int loopCount = 0;
+	public float scrollSpeed = 1;
+
+	public void GetHarder(){
+		scrollSpeed += .05f;
+		loopCount++;
+	}
+}
--- Assets/VertScrollingCode.cs
+++ Assets/VertScrollingCode.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class VertScrollingCode : MonoBehaviour {
 
-	public float speed;
 	public bool starting;
 	public VertScrollingCode otherOne;
 
@@ -23,10 +22,13 @@ public class VertScrollingCode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += new Vector3 (0, -speed, 0);
+		transform.position += new Vector3 (0, -Manager.Instance.scrollSpeed, 0);
 
 		if (transform.position.y <= lessFloat) {
 			transform.position = new Vector3 (0, setFloat, 0); 
+
+			Manager.Instance.GetHarder ();
+			Debug.Log ("Looped " + Manager.Instance.loopCount + " times");
 		}
 	}
 
