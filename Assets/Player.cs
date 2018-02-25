2780dcd90ad5252e956140f2935b14b1a8f58c77
Jonathan Cunningham <jnidaokefantagefacebook27@gmail.com>
2018-02-24 20:32:58 -0500

    Setup Enemy Anims and Test Map
    Created the animations for all the enemies. Set  up a second map that has test positions of all the enemies

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,16 +6,26 @@ public class Player : MonoBehaviour {
 
 	private Rigidbody2D rgbd;
 	public float speed;
+    public GameObject rockBullet;
 
-    public bool right, left, up, down;
+    private bool right, left, up, down;
 
 	// Use this for initialization
 	void Start () {
+        StartCoroutine(Shoot());
 		rgbd = gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject rock = Instantiate(rockBullet, transform.position, Quaternion.identity) as GameObject;
+            rock.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 7);
+            Destroy(rock, 5);
+        }
+
         //if (Input.GetAxis("Horizontal") > .3)
         if(Input.GetKey(KeyCode.RightArrow))
         {
@@ -54,5 +64,14 @@ public class Player : MonoBehaviour {
             rgbd.velocity = new Vector2(rgbd.velocity.x, -speed);
         else
             rgbd.velocity = new Vector2(rgbd.velocity.x, 0);
+    }
+
+    IEnumerator Shoot()
+    {
+        GameObject rock = Instantiate(rockBullet, transform.position, Quaternion.identity) as GameObject;
+        rock.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 7);
+        Destroy(rock, 5);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Shoot());
     }
 }
